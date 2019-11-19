710addc067937c8db22d10124fb2d9def5c76c02
Anu George <anugeorge.cst@gmail.com>
2019-11-19 17:54:15 +0530

    commit

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -2,6 +2,9 @@ import os
 import django
 
 from channels.routing import get_default_application
+from channels.layers import get_channel_layer
+
+channel_layer = get_channel_layer()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
 django.setup()
